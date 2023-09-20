c38366ae48b25d31b3eb27d760e51ce03c39de7d
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-09-20 18:06:47 +1000

    starting customfilter and searchbar comp

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
-export { Hero, CustomButton, Footer, Navbar };
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomeFilter";
+export { Hero, CustomButton, Footer, Navbar, SearchBar, CustomFilter };
