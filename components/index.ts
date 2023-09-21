d5edfd32e3b678a589726e2c041d579b7eeb785f
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-09-21 14:18:08 +1000

    headless.ui combobox

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,13 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomeFilter";
-export { Hero, CustomButton, Footer, Navbar, SearchBar, CustomFilter };
+import SearchManufacturer from "./SearchManufacturer";
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
