c4542e1dbb4861013520b8351c4ef8a32307744b
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-09-27 14:34:48 +1000

    creating use state

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
-import Image from "next/image";
 
-export default async function Home({ searchParams }) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2023,
-    fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
-    model: searchParams.model || "",
-  });
+export default function Home() {
+  const [allCars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(true);
+  //search states
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
+  // filter states
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2023);
+
+  //pagination states
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacturer || "",
+        year: year || 2023,
+        fuel: fuel || "",
+        limit: limit || 10,
+        model: model || "",
+      });
+
+      setAllCars(result);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, [fuel, year, limit, manufacturer, model]);
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
